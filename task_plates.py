3f51fd2d49ecc6907949bdf78970135db59395f7
tungyr <rotkivem@gmail.com>
2018-04-18 14:59:36 +0300

    Second task 18.04

---
+++ task_plates.py
@@ -0,0 +1,11 @@
+﻿plate = int(input())
+fairy = int(input())
+while plate >= 0 and fairy >= 0:
+    if fairy == 0 and plate != 0:
+        print("Моющее средство закончилось. Осталось", plate,"тарелок")
+    if plate == 0 and fairy != 0:
+        print ("Все тарелки вымыты. Осталось", fairy,"ед. моющего средства")
+    if plate == 0 and fairy == 0:
+        print ("Все тарелки вымыты, моющее средство закончилось")
+    fairy -= .5
+    plate -= 1
