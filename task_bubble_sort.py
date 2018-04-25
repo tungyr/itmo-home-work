5b326051801caf15ad5fb28b9e3fe85a13bfdb56
tungyr <rotkivem@gmail.com>
2018-04-25 14:38:18 +0300

    Revised bubble_sort task

--- task_bubble_sort.py
+++ task_bubble_sort.py
@@ -1,6 +1,6 @@
-def bubble_sort(lst):
-    for i in range(len(lst)):
-        for j in range(len(lst) - 1, i, -1):
+def bubble_sort(lst):   #объявление функции
+    for i in range(len(lst)):      #перебор диапазона с длиной lst
+        for j in range(len(lst) - 1, i, -1):      #перебор диапазона lst c обменом значений
             if lst[j] < lst[j-1]:
                 lst[j], lst[j-1] = lst[j-1], lst[j]
 
