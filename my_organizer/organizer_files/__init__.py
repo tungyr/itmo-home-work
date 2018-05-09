f8d946a481af3bd557519db50526a0fca854fb75
tungyr <rotkivem@gmail.com>
2018-05-09 17:53:03 +0300

    My organizer completed

--- my_organizer/organizer_files/__init__.py
+++ my_organizer/organizer_files/__init__.py
@@ -7,36 +7,86 @@ get_connection = lambda: storage.connect('organizer.sqlite')
 
 def action_add():
     """Добавить задачу"""
-    task = input ('\nВведите задачу: ')
+    task = input ('\nВведите новую задачу: ')
     status = ('undone')
     with get_connection() as conn:
         task = storage.add_task(conn, task, status)
+    print('\nЗадача добавлена: ', task)
+
 
 
 def action_find_all():
     """Вывести все задачи"""
     with get_connection() as conn:
-        print (storage.find_all(conn))
+        storage.find_all(conn)
+
+
+
+def action_find():
+    """Вывести задачи по параметрам"""
+
+    print('''
+    1. Вывести все задачи
+    2. Вывести завершенные задачи
+    3. Вывести незавершенные задачи
+    4. Вывести задачи на повторе
+    q. Выход в предыдущее меню
+    ''')
+
+    actions_find = {
+        '1': action_find_all,       # ключ словаря - параметр, по которому выбираются задачи в БД
+        '2': 'done',
+        '3': 'undone',
+        '4': 'repeat',
+        'q': main,
+        }
+
+    while 1:
+        cmd = input('\nВведите номер пункта: ')
+        action = actions_find.get(cmd)
+
+        if action == action_find_all or action == main:     # проверка выбора пользователем показа меню или показа всех задач без разбора
+            action()
+        elif action:
+            status_find = action
+            with get_connection() as conn:
+                storage.find_status(conn, status_find)
+        else:
+            print('Неизвестная команда')
+
 
 
 def action_edit():
-    rev_task_id = input ('\nTask ID for revise?: ')
-    rev_task = input ('\nRevise task: ')
+    rev_task_id = input ('\nID задачи для редактирования: ')
+    rev_task = input ('\nНовый текст задачи: ')
     with get_connection() as conn:
-        storage.task_edit(conn, rev_task_id, rev_task)
-
-
+        storage.task_edit(conn, rev_task, rev_task_id)
+    print('\nЗадача изменена: ', storage.task_edit(conn, rev_task, rev_task_id)) # Демонстрирование результата в подтверждение выполнения действия
 
 def action_complete():
     """Завершить задачу"""
-    compl_task_id = input ('\nTask ID for complete?: ')
-    status = 'done'
+    compl_task_id = input ('\nID завершенной задачи: ')
+    status = ('done')
     with get_connection() as conn:
-        storage.task_compl(conn, compl_task_id, status)
-
+        storage.task_compl(conn, status, compl_task_id)
+    print('\nЗадача завершена: ', storage.task_compl(conn, status, compl_task_id))
 
 def action_again():
     """Начать задачу сначала"""
+    repeat_task_id = input ('\nID задачи на повторение: ')
+    status = ('repeat')
+    with get_connection() as conn:
+        storage.task_repeat(conn, status, repeat_task_id)
+    print('\nЗадача изменена: ', storage.task_repeat(conn, status, repeat_task_id))
+
+def action_delete():
+    """Удаление задачи"""
+    delete_task_id = input ('\nID задачи для удаления: ')
+    with get_connection() as conn:
+        storage.task_delete(conn,delete_task_id)
+        
+
+
 
 def action_show_menu():
     """Показать меню"""
@@ -47,12 +97,14 @@ def action_show_menu():
 3. Отредактировать задачу
 4. Завершить задачу
 5. Начать задачу сначала
+6. Удалить задачу
 m. Показать меню
 q. Выйти
 ''')
 
 def action_exit():
     """Выйти"""
+    print("\nДо новых встреч!")
     sys.exit(0)      # завершает с 0-вым кодом - все хорошо
 
 
@@ -63,11 +115,12 @@ def main():
     action_show_menu()
 
     actions = {
-        '1': action_find_all,
+        '1': action_find,
         '2': action_add,
         '3': action_edit,
         '4': action_complete,
         '5': action_again,
+        '6': action_delete,
         'm': action_show_menu,
         'q': action_exit,
         }
--- my_organizer/organizer_files/storage.py
+++ my_organizer/organizer_files/storage.py
@@ -15,15 +15,17 @@ SQL_SELECT_ALL ='''
 
 SQL_SELECT_TASK = SQL_SELECT_ALL + ' WHERE task=?'
 SQL_SELECT_TASK_BY_PK = SQL_SELECT_ALL + ' WHERE id=?'
+SQL_SELECT_TASK_BY_STATUS = SQL_SELECT_ALL + ' WHERE status=?'
 
 SQL_EDIT_TASK = '''
     UPDATE organizer SET task=? WHERE id=?
 '''
 
 SQL_COMPL_TASK = '''
-    UPDATE organizer SET task=? WHERE id=?
+    UPDATE organizer SET status=? WHERE id=?
 '''
 
+SQL_DELETE_TASK = '''DELETE FROM organizer WHERE id=?'''
 
 #def connect (db_name=':memory:'):
 def connect(db_name=None):
@@ -37,20 +39,27 @@ def connect(db_name=None):
 
 def initialize(conn):
     script_path = Path.join(Path.dirname(__file__), 'schema.sql')
-
-
     with conn, open(script_path) as f:
         conn.executescript(f.read())
 
 def task_edit(conn, rev_task, rev_task_id):
     with conn:
-        cursor = conn.execute(SQL_EDIT_TASK, (rev_task, rev_task_id))
+        cursor = conn.execute(SQL_EDIT_TASK,(rev_task, rev_task_id))
+        cursor = conn.execute(SQL_SELECT_TASK_BY_PK, (rev_task_id,)) #выборка последней измененной задачи для демонстрирования результата
+        return cursor.fetchone()
 
-
-def task_compl(conn, coml_task_id, status):
+def task_compl(conn, status, compl_task_id):
     with conn:
-        cursor = conn.execute(SQL_COMPL_TASK, (coml_task_id, status))
+        cursor = conn.execute(SQL_COMPL_TASK,(status, compl_task_id))
+        cursor = conn.execute(SQL_SELECT_TASK_BY_PK, (compl_task_id,))
+        return cursor.fetchone()
 
+
+def task_repeat(conn, status, repeat_task_id):
+    with conn:
+        cursor = conn.execute(SQL_COMPL_TASK,(status, repeat_task_id))
+        cursor = conn.execute(SQL_SELECT_TASK_BY_PK, (repeat_task_id,))
+        return cursor.fetchone()
 
 def add_task(conn, task, status):
     """Добавляет задачу в БД"""
@@ -69,18 +78,46 @@ def add_task(conn, task, status):
         pk = cursor.lastrowid
 
         conn.execute(SQL_UPDATE_TASK, (task, status, pk))
-
-        return task
+        cursor = conn.execute(SQL_SELECT_TASK_BY_PK, (pk,))
+        return task, cursor.fetchone()
 
 def find_all(conn):
     with conn:
         cursor = conn.execute(SQL_SELECT_ALL)
-        return cursor.fetchall()
+        rows = cursor.fetchall()
+        print ("\nСписок всех задач: ")
+        for row in rows:                        # представление списка задач в столбик
+            print(row)
 
+def find_status(conn, status_find):
+    with conn:
+        cursor = conn.execute(SQL_SELECT_TASK_BY_STATUS,(status_find,))
+        rows = cursor.fetchall()
+        print ("\nСписок запрошенных задач: ")
+        for row in rows:
+            print(row)
+
+def task_delete(conn,delete_task_id):
+    with conn:
+        found = find_task_pk(conn, delete_task_id)
+
+        if found:
+            with conn:
+                cursor = conn.execute(SQL_DELETE_TASK,(delete_task_id))
+                print('Задача удалена!')
+                find_all(conn)
+        else:
+                print('\nНет такой задачи!')
 
 def find_task(conn, task):
     """Возвращает task"""
 
     with conn:
         cursor = conn.execute(SQL_SELECT_TASK, (task,))
+        return cursor.fetchone()
+
+def find_task_pk(conn, id):
+    """Возвращает task по ID"""
+    with conn:
+        cursor = conn.execute(SQL_SELECT_TASK_BY_PK, (id,))
         return cursor.fetchone()
